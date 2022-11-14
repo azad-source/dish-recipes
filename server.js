7c7b82573a776909f211cd02641ac3a7459e6f8f
Azad Mamedov <azad_63_mamedov@mail.ru>
2022-11-14 11:42:54 +0500

    Secure connect to db

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dbInfo = require("access");
 
 const RecipeRoute = require("./routes/recipe");
 
@@ -12,10 +13,14 @@ const dbName = "dish-recipes";
 
 const mongoUrl = isDev
   ? `mongodb://localhost:27017/${dbName}`
-  : `mongodb://AdminCherry:Krakazyabra222723@83.136.233.139:27017/${dbName}`;
+  : `mongodb://azad-source.online:27017/${dbName}`; // 83.136.233.139:27017
+
+const { user, pass } = dbInfo;
 
 mongoose
   .connect(mongoUrl, {
+    user,
+    pass,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
