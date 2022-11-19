f9debc7703fc311d6ee9e2c035799fb1bd28e4c0
Azad Mamedov <azad_63_mamedov@mail.ru>
2022-11-19 17:20:01 +0500

    setting http and https

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dbInfo = require("./access.ts");
+
+const domain = "azad-source.online";
+
+// Certificate
+const privateKey = fs.readFileSync(
+  `/etc/letsencrypt/live/${domain}/privkey.pem`,
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  `/etc/letsencrypt/live/${domain}/cert.pem`,
+  "utf8"
+);
+const ca = fs.readFileSync(`/etc/letsencrypt/live/${domain}/chain.pem`, "utf8");
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 const RecipeRoute = require("./routes/recipe");
 
@@ -13,7 +35,7 @@ const dbName = "dish-recipes";
 
 const mongoUrl = isDev
   ? `mongodb://localhost:27017/${dbName}`
-  : `mongodb://azad-source.online:27017/${dbName}`; // 83.136.233.139:27017
+  : `mongodb://${domain}:27017/${dbName}`; // 83.136.233.139:27017
 
 const { user, pass } = dbInfo;
 
@@ -38,10 +60,22 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 app.use("/api/recipe", RecipeRoute);
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
+});
+
+httpsServer.listen(443, () => {
+  console.log("HTTPS Server running on port 443");
+});
